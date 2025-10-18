db060a68d89c0b658590ec112507d9d3671ea198
Kareem Najjar <jexxlink@gmail.com>
2025-10-17 19:28:02 -0600

    primer commit

---
+++ Bootloader.js
@@ -0,0 +1,32 @@
+class Bootloader extends Phaser.Scene{
+    constructor(){
+        super({
+            key: "Bootloader" //Nombre interno o clave de referencia
+        });
+    }
+
+    init() {
+        
+        console.log("Soy init");
+    }
+
+    preload() {
+
+        
+    
+    }
+
+    create() {
+
+
+
+    }
+
+    update(time, delta) {
+
+        
+
+    }
+}
+
+export default Bootloader;
